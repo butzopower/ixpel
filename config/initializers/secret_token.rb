5b70fe65170f94686db5c9707cb7f634b182053a
Brian Butz <butzopower@gmail.com>
2012-11-25 18:17:30 -0800

    Initial rails new (already failed)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ixpel::Application.config.secret_token = 'ec919bbccab8278eef62462c4ebccdcd17d13ab0395f92ccf3b38eb0511c5be8e7a16d365f675f88b7a0ee2ccb5224d52d4a114335fe24894e9af569d367c2e0'
